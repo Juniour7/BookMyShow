3006b9e745efc929189606573fdffcee7f090049
Juniour7 <giftmomanyi97@gmail.com>
2023-08-11 16:52:53 +0300

    Edited the footer

--- src/components/Footer/footer.component.js
+++ src/components/Footer/footer.component.js
@@ -13,19 +13,30 @@ const Footer = () => {
                     <GiHut />
                     </span>
                     <h5 className="flex text-xl text-white text-bold">List your show. Got a show, event, activity or a great experience? Partner with us & get listed on bookmyshow</h5>
-                    <button className="bg-gift-50 text-white px-3 py-3 text-bold rounded-sm">Contact Today!</button>
+                    <button className="bg-gift-50 text-white text-md px-3 py-3 text-bold hover:cursor-pointer rounded-sm">Contact Today!</button>
                 </div>
                 <hr />
-                <div className="flex items-center gap-10">
-                   <span className="w-30">
-                    <BiLogoTwitter className="w-full h-full" />
-                   </span>
-                   <span>
-                    <BiLogoFacebookSquare />
-                   </span>
-                   <span>
-                    <BiLogoInstagram />
-                   </span>
+                <div className="text-gray-600 text-bold text-md px-3 leading-loose">
+                   <h4 className="text-lg">MOVIES NOW SHOWING IN NAIROBI</h4>
+                   <p>The Katha continues | OMG 2 | Rocky Aur Rani Kii Prem Kahaani | Jailer | Oppenheimer | Meg2: The French | Mission Impossible: Dead Reckoning - Part One | Barbie | Baipan Bhari Deva | Bhola Shankar </p>
+                   <h4 className="text-lg">MOVIES NOW SHOWING IN NAIROBI</h4>
+                   <p>The Katha continues | OMG 2 | Rocky Aur Rani Kii Prem Kahaani | Jailer | Oppenheimer | Meg2: The French | Mission Impossible: Dead Reckoning - Part One | Barbie | Baipan Bhari Deva | Bhola Shankar </p>
+                   <h4 className="text-lg">MOVIES NOW SHOWING IN NAIROBI</h4>
+                   <p>The Katha continues | OMG 2 | Rocky Aur Rani Kii Prem Kahaani | Jailer | Oppenheimer | Meg2: The French | Mission Impossible: Dead Reckoning - Part One | Barbie | Baipan Bhari Deva | Bhola Shankar </p>
+                   <h4 className="text-lg">MOVIES NOW SHOWING IN NAIROBI</h4>
+                   <p>The Katha continues | OMG 2 | Rocky Aur Rani Kii Prem Kahaani | Jailer | Oppenheimer | Meg2: The French | Mission Impossible: Dead Reckoning - Part One | Barbie | Baipan Bhari Deva | Bhola Shankar </p>
+                   <h4 className="text-lg">MOVIES NOW SHOWING IN NAIROBI</h4>
+                   <p>The Katha continues | OMG 2 | Rocky Aur Rani Kii Prem Kahaani | Jailer | Oppenheimer | Meg2: The French | Mission Impossible: Dead Reckoning - Part One | Barbie | Baipan Bhari Deva | Bhola Shankar </p>
+                   <h4 className="text-lg">MOVIES NOW SHOWING IN NAIROBI</h4>
+                   <p>The Katha continues | OMG 2 | Rocky Aur Rani Kii Prem Kahaani | Jailer | Oppenheimer | Meg2: The French | Mission Impossible: Dead Reckoning - Part One | Barbie | Baipan Bhari Deva | Bhola Shankar </p>
+                   <h4 className="text-lg">MOVIES NOW SHOWING IN NAIROBI</h4>
+                   <p>The Katha continues | OMG 2 | Rocky Aur Rani Kii Prem Kahaani | Jailer | Oppenheimer | Meg2: The French | Mission Impossible: Dead Reckoning - Part One | Barbie | Baipan Bhari Deva | Bhola Shankar </p>
+                   <h4 className="text-lg">MOVIES NOW SHOWING IN NAIROBI</h4>
+                   <p>The Katha continues | OMG 2 | Rocky Aur Rani Kii Prem Kahaani | Jailer | Oppenheimer | Meg2: The French | Mission Impossible: Dead Reckoning - Part One | Barbie | Baipan Bhari Deva | Bhola Shankar </p>
+                   <h4 className="text-lg">MOVIES NOW SHOWING IN NAIROBI</h4>
+                   <p>The Katha continues | OMG 2 | Rocky Aur Rani Kii Prem Kahaani | Jailer | Oppenheimer | Meg2: The French | Mission Impossible: Dead Reckoning - Part One | Barbie | Baipan Bhari Deva | Bhola Shankar </p>
+                   <h4 className="text-lg">MOVIES NOW SHOWING IN NAIROBI</h4>
+                   <p>The Katha continues | OMG 2 | Rocky Aur Rani Kii Prem Kahaani | Jailer | Oppenheimer | Meg2: The French | Mission Impossible: Dead Reckoning - Part One | Barbie | Baipan Bhari Deva | Bhola Shankar </p>
                 </div>
             </div>
            </footer>
